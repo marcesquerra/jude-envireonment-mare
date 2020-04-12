257f33d6705d021a6e41ba5891c6ad73c1dabe05
Marc Esquerra <esquerra@gmail.com>
2020-04-12 16:30:23 +0100

    Add new set of default compiler settings

--- src/main/scala/com/bryghts/jude/mare/MarePlugin.scala
+++ src/main/scala/com/bryghts/jude/mare/MarePlugin.scala
@@ -1,3 +1,5 @@
+// format: off
+
 package com.bryhts.jude.mare
 
 import java.io.File
@@ -30,12 +32,33 @@ object MarePlugin extends AutoPlugin {
     libraryDependencies += "com.bryghts" %%% "high-priority" % "v4C646",
     libraryDependencies += "com.github.ghik" % "silencer-lib" % silencerVersion % Provided cross CrossVersion.full,
     scalacOptions ++= Seq(
-      "-encoding",
-      "utf8",
-      "-deprecation",
-      "-unchecked",
-      "-language:higherKinds",
-      "-Yno-imports"
+      "-Yno-imports",
+      "-deprecation",                      // Emit warning and location for usages of deprecated APIs.
+      "-encoding", "utf-8",                // Specify character encoding used by source files.
+      "-feature",                          // Emit warning and location for usages of features that should be imported explicitly.
+      "-language:higherKinds",             // Allow higher-kinded types
+      "-unchecked",                        // Enable additional warnings where generated code depends on assumptions.
+      "-Xcheckinit",                       // Wrap field accessors to throw an exception on uninitialized access.
+      "-Xlint:adapted-args",               // Warn if an argument list is modified to match the receiver.
+      "-Yno-adapted-args",                 // Do not adapt an argument list (either by inserting () or creating a tuple) to match the receiver.
+      "-Xlint:delayedinit-select",         // Selecting member of DelayedInit.
+      "-Xlint:doc-detached",               // A Scaladoc comment appears to be detached from its element.
+      "-Xlint:inaccessible",               // Warn about inaccessible types in method signatures.
+      "-Xlint:infer-any",                  // Warn when a type argument is inferred to be `Any`.
+      "-Xlint:nullary-override",           // Warn when non-nullary `def f()' overrides nullary `def f'.
+      "-Xlint:nullary-unit",               // Warn when nullary methods return Unit.
+      "-Xlint:private-shadow",             // A private field (or class parameter) shadows a superclass field.
+      "-Xlint:type-parameter-shadow",      // A local type parameter shadows a type already in scope.
+      "-Xlint:unsound-match",              // Pattern match may not be typesafe.
+      "-Ypartial-unification",             // Enable partial unification in type constructor inference
+      "-Ywarn-dead-code",                  // Warn when dead code is identified.
+      "-Ywarn-inaccessible",               // Warn about inaccessible types in method signatures.
+      "-Ywarn-infer-any",                  // Warn when a type argument is inferred to be `Any`.
+      "-Ywarn-nullary-override",           // Warn when non-nullary `def f()' overrides nullary `def f'.
+      "-Ywarn-nullary-unit",               // Warn when nullary methods return Unit.
+      "-Ywarn-value-discard",              // Warn when non-Unit expression results are unused.
+      "-Xlint:stars-align",                // Pattern sequence wildcard must align with sequence component.
+      "-Ywarn-unused-import"
     ),
     resolvers += Resolver.bintrayRepo("bryghts", "jude"),
     addCompilerPlugin(
