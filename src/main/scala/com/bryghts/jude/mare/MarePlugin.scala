1b6da51b0c78645ad5a67bc10942057e6edd7f9b
Marc Esquerra <esquerra@gmail.com>
2020-04-12 17:39:02 +0100

    Pattern matching to require Eq instances

--- src/main/scala/com/bryghts/jude/mare/MarePlugin.scala
+++ src/main/scala/com/bryghts/jude/mare/MarePlugin.scala
@@ -65,7 +65,7 @@ object MarePlugin extends AutoPlugin {
       "org.scalamacros" % "paradise" % "2.1.1" cross CrossVersion.full
     ),
     addCompilerPlugin("com.bryghts.jude" %% "jude-importer" % "vA9DE7"),
-    addCompilerPlugin("com.bryghts.jude" %% "jude-renamer" % "v16C61"),
+    addCompilerPlugin("com.bryghts.jude" %% "jude-renamer" % "v82737"),
     addCompilerPlugin("com.bryghts.jude" %% "jude-literals" % "vD3788"),
     addCompilerPlugin(
       "com.github.ghik" % "silencer-plugin" % silencerVersion cross CrossVersion.full
